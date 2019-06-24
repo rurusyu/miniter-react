04982ed5c7b182441850ae07b039e1cefb8db88a
rurusyu <Chrizard006@protonmail.com>
2019-06-24 12:57:08 +0900

    This fixes #1

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -4,16 +4,15 @@ import { withRouter } from 'react-router-dom';
 
 class main extends React.Component {     
   
+  componentDidMount(){
+    setTimeout(() => {
+      this.props.history.push("/login")        
+    }, 3000)
+  }
+
   render(){
     return(
-        <>
         <h1>메인화면으로 이동 중입니다.</h1>
-       { 
-        setTimeout(() => {
-          this.props.history.push("/login")        
-        }, 3000)
-       }
-        </>
     )
   }
 }
